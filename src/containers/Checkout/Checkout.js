811c5e51bb2d5f9f3c504f3c93baf557d74b750f
CAM603 <cameronhawley@comcast.net>
2020-09-03 08:08:30 -0400

    add contact input

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import { Route } from "react-router-dom";
+import ContactData from "./ContactData/ContactData";
 
 class Checkout extends Component {
     state = {
@@ -16,7 +18,7 @@ class Checkout extends Component {
         const ingredients = {};
         for (let param of query.entries()) {
             // ['salad','1']
-            ingredients[param[0]] = ingredients[param[1]];
+            ingredients[param[0]] = +param[1];
         }
         this.setState({ ingredients: ingredients });
     }
@@ -34,6 +36,10 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}
+                />
+                <Route
+                    path={this.props.match.path + "/contact-data"}
+                    component={ContactData}
                 />
             </div>
         );
